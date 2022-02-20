c41e5a9dffc00b72af897288af158d5de06c5582
Cosmin Tanislav <demonsingur@gmail.com>
2022-02-20 19:21:09 +0200

    marble: Add Xiaomi sensor module
    Change-Id: I7196dce967c9adbc62bb5156ade749c92c7780a1
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- device.mk
+++ device.mk
@@ -437,7 +437,11 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    marbleLightSensor
+    marbleLightSensor \
+    sensors.xiaomi
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
 PRODUCT_PACKAGES += \
     android.hardware.sensors@2.1-service.xiaomi-multihal \
