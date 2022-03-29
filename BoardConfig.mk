998f76813fadde682c0a52cf5457d7f9252f1f2d
LuK1337 <priv.luk@gmail.com>
2022-03-30 00:30:07 +0200

    marble: Include vendor/debugfs.config
    This disables the following kernel configs:
    * CONFIG_DEBUG_FS
    * CONFIG_PAGE_OWNER
    
    Change-Id: Idbf74e3dee4dccff4708462271e79374e28449ae
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -105,7 +105,8 @@ TARGET_KERNEL_CONFIG := \
     gki_defconfig \
     vendor/waipio_GKI.config \
     vendor/xiaomi_GKI.config \
-    vendor/marble_GKI.config
+    vendor/marble_GKI.config \
+    vendor/debugfs.config
 
 BOARD_BOOT_HEADER_VERSION := 4
 BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOT_HEADER_VERSION)
