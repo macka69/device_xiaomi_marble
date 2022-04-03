3ee66838f38f0ca2df9382b9ceff447eca74b307
AdarshGrewal <adarshgrewal@gmail.com>
2022-04-03 19:18:28 +0530

    marble: Include MiuiCamera if exists
    Change-Id: Iee675b4924cb2be7ac2d36c7737b06edc9cadfc9

--- device.mk
+++ device.mk
@@ -130,6 +130,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES_DEBUG += \
     bootctl:64
 
+# Call the MiuiCamera setup
+$(call inherit-product-if-exists, device/xiaomi/miuicamera-marble/device.mk)
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.7.vendor:64 \
