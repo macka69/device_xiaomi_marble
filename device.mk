e7fa82df25e3b0f765f3780a0b877b749999ad01
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-10-04 23:09:51 +0200

    marble: Reorder Soong namespace includes
    Change-Id: Ia90e559d838d9f4b41ddc77f54177e7940ebc313
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- device.mk
+++ device.mk
@@ -346,7 +346,8 @@ PRODUCT_COPY_FILES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/xiaomi
+    hardware/xiaomi \
+    vendor/qcom/opensource/usb/etc
 
 # System properties
 PRODUCT_COPY_FILES += \
@@ -425,8 +426,6 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
     usb_compositions.conf
-
-PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/usb/etc
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
