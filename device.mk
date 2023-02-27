6c028dec2dfff906fa438b53116f27aa1d32ccd6
Kumar Anurag Singh <quic_kumaranu@quicinc.com>
2023-02-27 02:15:26 -0800

    marble: audio: Remove direct flag for VOIP RX
    Remove direct flag for VOIP RX.
    
    Fixes the problem of no sound during a VoIP call and the arrival of a notification at this time.
    How to reproduce:
    1. Make a voice or video call (WhatsApp, Telegram etc..)
    2. If a notification is received during your conversation,
       you will stop hearing the other person. The interlocutor will continue to hear you.
    
    Change-Id: I9ee6f300b7a891afd1ad06c063424509d16d271e

--- device.mk
+++ device.mk
@@ -82,6 +82,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_ukee/audio_effects.xml \
+    $(LOCAL_PATH)/audio/audio_io_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_io_policy.conf \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_ukee/audio_policy_configuration.xml
 
 PRODUCT_COPY_FILES += \
