7bad3e6ec8f87305614bdc28a7b49b8bce1709a9
bheatleyyy <sugakesshaa@pixelos.net>
2024-03-14 09:08:32 +0100

    marble: Add support for Dolby Atmos
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: Ia97eb58c63d02e95f546fd35874d0201525331c2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -248,6 +248,7 @@ ODM_MANIFEST_SKUS += marble
 ODM_MANIFEST_MARBLE_FILES := $(DEVICE_PATH)/vintf/manifest_nfc.xml
 
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(DEVICE_PATH)/vintf/device_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
     vendor/aosp/config/device_framework_matrix.xml
