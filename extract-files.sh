19e87d3121e8ccd8d86d43f3cf2e09d79847eda2
Fiqri Ardyansyah <fiqri191002@gmail.com>
2024-04-30 18:52:15 +0700

    marble: Stop shipping stock firmware
    Let users flash the firmware manually.
    
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -31,29 +31,24 @@ source "${HELPER}"
 # Default to sanitizing the vendor folder before extraction
 CLEAN_VENDOR=true
 
-ONLY_FIRMWARE=
 KANG=
 SECTION=
 
 while [ "${#}" -gt 0 ]; do
     case "${1}" in
-        --only-firmware)
-            ONLY_FIRMWARE=true
-            ;;
-        -n | --no-cleanup)
-            CLEAN_VENDOR=false
-            ;;
-        -k | --kang)
-            KANG="--kang"
-            ;;
-        -s | --section)
-            SECTION="${2}"
-            shift
-            CLEAN_VENDOR=false
-            ;;
-        *)
-            SRC="${1}"
-            ;;
+        -n | --no-cleanup )
+                CLEAN_VENDOR=false
+                ;;
+        -k | --kang )
+                KANG="--kang"
+                ;;
+        -s | --section )
+                SECTION="${2}"; shift
+                CLEAN_VENDOR=false
+                ;;
+        * )
+                SRC="${1}"
+                ;;
     esac
     shift
 done
@@ -142,12 +137,6 @@ function blob_fixup_dry() {
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
-if [ -z "${ONLY_FIRMWARE}" ]; then
-    extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
-fi
-
-if [ -z "${SECTION}" ]; then
-    extract_firmware "${MY_DIR}/proprietary-firmware.txt" "${SRC}"
-fi
+extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 
 "${MY_DIR}/setup-makefiles.sh"
