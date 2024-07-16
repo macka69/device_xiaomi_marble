3cd3bb527b4f3a344703c5cad4c3fdd86ac2e798
John Macka <macka2004@gmail.com>
2024-07-16 12:24:19 +1000

    marble: bring up Parasite project

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2022-2023 The LineageOS Project
+# Copyright (C) 2022-2023 The aospOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_marble.mk
+    $(LOCAL_DIR)/aosp_marble.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_marble-user \
-    lineage_marble-userdebug \
-    lineage_marble-eng
+    aosp_marble-user \
+    aosp_marble-userdebug \
+    aosp_marble-eng
