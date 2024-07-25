697710b3e9d32c06d180e5563f48fe6d84e4dc27
Cosmin Tanislav <demonsingur@gmail.com>
2024-07-25 20:01:51 +0300

    marble: remove unused WiFi symlink
    /vendor/etc/wifi/WCNSS_qcom_cfg.ini is not present on stock.
    
    Change-Id: I750aa7a9f10aab2d24cd4f69404d9ce315834aa7
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- device.mk
+++ device.mk
@@ -509,6 +509,5 @@ PRODUCT_COPY_FILES += \
 
 # WiFi firmware symlinks
 PRODUCT_PACKAGES += \
-    firmware_WCNSS_qcom_cfg.ini_symlink \
     firmware_qca6490_WCNSS_qcom_cfg.ini_symlink \
     firmware_qca6490_wlan_mac.bin_symlink
