dbbdf57ff16070443519a1eeb6f808f31d46a7e0
Fiqri Ardyansyah <fiqri191002@gmail.com>
2024-08-16 14:51:48 +0700

    Revert "marble: overlay: Allow switching between 60Hz and 120Hz"
    Already done in commit 2567a5a33c9b481ef6524394bca0626c1f4a7e63.
    
    This reverts commit 8c6fdb8377961cb0abae3ffc60a4c752f51bc830.
    
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- device.mk
+++ device.mk
@@ -369,7 +369,6 @@ PRODUCT_PACKAGES += \
     NfcResMarble \
     SettingsProviderResMarble \
     SettingsProviderResMarbleCN \
-    SettingsResMarble \
     SystemUIResMarble \
     WifiResMarble \
     WifiResMarbleCN \
