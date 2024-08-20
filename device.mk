f833a09f51b00a94934f4d5fe35ad067bcb7e8b5
Aaron Kling <webgeek1234@gmail.com>
2024-08-19 23:48:40 -0500

    marble: Remove omx libraries
    This arch uses exclusively c2
    
    Change-Id: I306464bc3d9065b557bcf3e024c43282f82e5fe1

--- device.mk
+++ device.mk
@@ -267,8 +267,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.qti.media.rc \
     init.qti.media.sh \
-    libOmxCore \
-    libstagefrighthw
 
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
