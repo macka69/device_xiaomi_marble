9c5d7058c199070c083841fa18d79274d87d9efb
Arian <arian.kulmer@web.de>
2024-10-13 15:54:03 +0200

    marble: Use patchelf 0.17.2 as the default patchelf version
    This will also be used for the utility functions like FIX_SONAME
    
    Change-Id: I0b2ac5b601d24c5cc3626ca41d2fad0e662ac72f
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -21,6 +21,10 @@ export TARGET_ENABLE_CHECKELF="true"
 # Can still be used with broken XML files by using blob_fixup
 export TARGET_DISABLE_XML_FIXING=true
 
+# Define the default patchelf version used to patch blobs
+# This will also be used for utility functions like FIX_SONAME
+export PATCHELF_VERSION=0_17_2
+
 HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
@@ -61,18 +65,18 @@ function blob_fixup() {
     case "${1}" in
         system_ext/lib64/libwfdservice.so)
             [ "$2" = "" ] && return 0
-            "${PATCHELF_0_17_2}" --replace-needed "android.media.audio.common.types-V2-cpp.so" "android.media.audio.common.types-V3-cpp.so" "${2}"
+            "${PATCHELF}" --replace-needed "android.media.audio.common.types-V2-cpp.so" "android.media.audio.common.types-V3-cpp.so" "${2}"
             ;;
         vendor/bin/hw/android.hardware.security.keymint-service-qti|vendor/lib64/libqtikeymint.so)
             [ "$2" = "" ] && return 0
-            "${PATCHELF_0_17_2}" --replace-needed "android.hardware.security.keymint-V1-ndk_platform.so" "android.hardware.security.keymint-V1-ndk.so" "${2}"
-            "${PATCHELF_0_17_2}" --replace-needed "android.hardware.security.secureclock-V1-ndk_platform.so" "android.hardware.security.secureclock-V1-ndk.so" "${2}"
-            "${PATCHELF_0_17_2}" --replace-needed "android.hardware.security.sharedsecret-V1-ndk_platform.so" "android.hardware.security.sharedsecret-V1-ndk.so" "${2}"
-            grep -q "android.hardware.security.rkp-V1-ndk.so" "${2}" || "${PATCHELF_0_17_2}" --add-needed "android.hardware.security.rkp-V1-ndk.so" "${2}"
+            "${PATCHELF}" --replace-needed "android.hardware.security.keymint-V1-ndk_platform.so" "android.hardware.security.keymint-V1-ndk.so" "${2}"
+            "${PATCHELF}" --replace-needed "android.hardware.security.secureclock-V1-ndk_platform.so" "android.hardware.security.secureclock-V1-ndk.so" "${2}"
+            "${PATCHELF}" --replace-needed "android.hardware.security.sharedsecret-V1-ndk_platform.so" "android.hardware.security.sharedsecret-V1-ndk.so" "${2}"
+            grep -q "android.hardware.security.rkp-V1-ndk.so" "${2}" || "${PATCHELF}" --add-needed "android.hardware.security.rkp-V1-ndk.so" "${2}"
             ;;
         vendor/bin/qcc-trd)
             [ "$2" = "" ] && return 0
-            "${PATCHELF_0_17_2}" --replace-needed "libgrpc++_unsecure.so" "libgrpc++_unsecure_prebuilt.so" "${2}"
+            "${PATCHELF}" --replace-needed "libgrpc++_unsecure.so" "libgrpc++_unsecure_prebuilt.so" "${2}"
             ;;
         vendor/etc/camera/marble_enhance_motiontuning.xml|vendor/etc/camera/marble_motiontuning.xml)
             [ "$2" = "" ] && return 0
@@ -92,23 +96,23 @@ function blob_fixup() {
             ;;
         vendor/lib64/libcamximageformatutils.so)
             [ "$2" = "" ] && return 0
-            "${PATCHELF_0_17_2}" --replace-needed "vendor.qti.hardware.display.config-V2-ndk_platform.so" "vendor.qti.hardware.display.config-V2-ndk.so" "${2}"
+            "${PATCHELF}" --replace-needed "vendor.qti.hardware.display.config-V2-ndk_platform.so" "vendor.qti.hardware.display.config-V2-ndk.so" "${2}"
             ;;
         vendor/bin/hw/vendor.dolby.hardware.dms@2.0-service)
             [ "$2" = "" ] && return 0
-            "${PATCHELF_0_17_2}" --add-needed "libstagefright_foundation-v33.so" "${2}"
+            "${PATCHELF}" --add-needed "libstagefright_foundation-v33.so" "${2}"
             ;;
         vendor/lib/c2.dolby.client.so)
             [ "$2" = "" ] && return 0
-            "${PATCHELF_0_17_2}" --add-needed "libcodec2_hidl_shim.so" "${2}"
+            "${PATCHELF}" --add-needed "libcodec2_hidl_shim.so" "${2}"
             ;;
         vendor/lib64/c2.dolby.client.so)
             [ "$2" = "" ] && return 0
-            "${PATCHELF_0_17_2}" --add-needed "libcodec2_hidl_shim.so" "${2}"
+            "${PATCHELF}" --add-needed "libcodec2_hidl_shim.so" "${2}"
             ;;    
         vendor/lib64/hw/audio.primary.taro.so)
             [ "$2" = "" ] && return 0
-            "${PATCHELF_0_17_2}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
+            "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
             ;;
         *)
             return 1
