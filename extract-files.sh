ef96e8dd76e987572b4cd0c48bd711064b24a8f2
Arian <arian.kulmer@web.de>
2024-10-17 16:09:49 +0700

    Revert "Rename audio.primary.taro to audio.primary.taro-marble to avoid conflicts"
    This reverts commit 8879e9264782175212a9c617be201547b0d04f80.
    
    Change-Id: I6300bdf18f59131eb7400997b5bcabe0b7d3a831
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -94,10 +94,6 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             "${PATCHELF_0_17_2}" --replace-needed "vendor.qti.hardware.display.config-V2-ndk_platform.so" "vendor.qti.hardware.display.config-V2-ndk.so" "${2}"
             ;;
-        vendor/lib64/libkaraokepal.so)
-            [ "$2" = "" ] && return 0
-            "${PATCHELF_0_17_2}" --replace-needed "audio.primary.taro.so" "audio.primary.taro-marble.so" "${2}"
-            ;;
         vendor/bin/hw/vendor.dolby.hardware.dms@2.0-service)
             [ "$2" = "" ] && return 0
             "${PATCHELF_0_17_2}" --add-needed "libstagefright_foundation-v33.so" "${2}"
