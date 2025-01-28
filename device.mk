37aeec396d8e8ded109de829327ead4706dc2021
Fiqri Ardyansyah <fiqri191002@gmail.com>
2025-01-28 12:39:01 +0700

    marble: Rearrange overlays
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- device.mk
+++ device.mk
@@ -270,18 +270,20 @@ PRODUCT_PACKAGES += \
     SettingsResMarble \
     SystemUIResMarble \
     WifiResMarble \
-    WifiResMarbleCN \
+    WifiResMarbleCN
 
 PRODUCT_PACKAGES += \
-    DialerResXiaomi \
     FrameworksResTarget \
-    FrameworksResXiaomi \
-    LineageResXiaomi \
-    SettingsProviderResXiaomi \
-    SettingsResXiaomi \
     WifiResTarget \
     WifiResTarget_cape \
     WifiResTarget_spf
+
+PRODUCT_PACKAGES += \
+    DialerResXiaomi \
+    FrameworksResXiaomi \
+    LineageResXiaomi \
+    SettingsProviderResXiaomi \
+    SettingsResXiaomi
 
 # Partitions
 PRODUCT_PACKAGES += \
