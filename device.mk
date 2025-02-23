ed0365643b66e2fee8d8064fb388705854b5034c
Arian <arian.kulmer@web.de>
2025-02-23 12:05:18 +0100

    marble: rootdir: Drop useless init.qcom.class_core.sh
    This did nothing for us:
    `zeus:/ # bash -x /vendor/bin/init.qcom.class_core.sh
    ++ getprop ro.board.platform
    + target=taro
    + '[' -f /sys/devices/soc0/hw_platform ']'
    ++ cat /sys/devices/soc0/hw_platform
    + soc_hwplatform=MTP
    + '[' -f /sys/devices/soc0/soc_id ']'
    ++ cat /sys/devices/soc0/soc_id
    + soc_hwid=457
    + '[' -f /sys/devices/soc0/platform_version ']'
    ++ cat /sys/devices/soc0/platform_version
    + soc_hwver=65536
    + case "$target" in`
    
    Change-Id: I6e0f4b123387582975b15c0c39272877909739bb
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- device.mk
+++ device.mk
@@ -450,7 +450,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/bin/init.class_main.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.class_main.sh \
     $(LOCAL_PATH)/rootdir/bin/init.kernel.post_boot-cape.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.kernel.post_boot-cape.sh \
     $(LOCAL_PATH)/rootdir/bin/init.kernel.post_boot.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.kernel.post_boot.sh \
-    $(LOCAL_PATH)/rootdir/bin/init.qcom.class_core.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qcom.class_core.sh \
     $(LOCAL_PATH)/rootdir/bin/init.qcom.early_boot.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qcom.early_boot.sh \
     $(LOCAL_PATH)/rootdir/bin/init.qcom.post_boot.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qcom.post_boot.sh \
     $(LOCAL_PATH)/rootdir/bin/init.qcom.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qcom.sh \
