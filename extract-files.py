e2ee7c6fc9b69c3bd16a7d00f942bfacaff575f0
Fiqri Ardyansyah <fiqri191002@gmail.com>
2025-03-16 23:42:41 +0700

    marble: Address missing SetTaskProfiles symbol in camera libs
    The camera HAL forced stop because it could not load
    /vendor/lib64/hw/camera.xiaomi.so which is due to the missing
    `SetTaskProfiles` symbol referenced in several libraries:
    
    fiqri@Fiqri:~/proprietary_vendor_xiaomi_marble$ grep -i -r SetTaskProfiles
    grep: proprietary/vendor/lib64/hw/com.qti.chi.override.so: binary file matches
    grep: proprietary/vendor/lib64/libmialgoengine.so: binary file matches
    grep: proprietary/vendor/lib64/libcamxcommonutils.so: binary file matches
    
    This is because DECLS was removed in [1] on Android 15 QPR2, and
    caused SetTaskProfiles() to be hidden, so shim SetTaskProfiles
    with libprocessgroup_shim.
    
    [1]: https://cs.android.com/android/_/android/platform/system/core/+/3e7c17a8e7ad7275ea3a65642608b1c0becb38ad
    
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -108,6 +108,11 @@ blob_fixups: blob_fixups_user_type = {
         .add_line_if_missing('gettid: 1'),
     'vendor/etc/vintf/manifest/c2_manifest_vendor.xml': blob_fixup()
         .regex_replace('.+dolby.+\n', ''),
+    (
+        'vendor/lib64/hw/com.qti.chi.override.so',
+        'vendor/lib64/libcamxcommonutils.so',
+        'vendor/lib64/libmialgoengine.so',
+    ): blob_fixup().add_needed('libprocessgroup_shim.so'),
     'vendor/lib64/libcamximageformatutils.so': blob_fixup().replace_needed(
         'vendor.qti.hardware.display.config-V2-ndk_platform.so',
         'vendor.qti.hardware.display.config-V2-ndk.so',
