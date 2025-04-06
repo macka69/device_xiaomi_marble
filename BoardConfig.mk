77e88db218a2ed28d828bee5cd34d82c67a7e365
Fiqri Ardyansyah <fiqri191002@gmail.com>
2025-04-06 22:47:35 +0700

    marble: Update blobs from cupid OS2.0.1.0.VLCMIXM
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -198,7 +198,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 TARGET_SCREEN_DENSITY := 440
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2025-02-01
+VENDOR_SECURITY_PATCH := 2025-03-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
