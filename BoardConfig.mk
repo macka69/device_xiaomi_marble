48f26e0e30a7f160507932f919982b386e302090
John Macka <macka2004@gmail.com>
2025-04-23 09:08:47 +1000

    marble: drop Boardconfig reserve size

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -170,8 +170,6 @@ TARGET_COPY_OUT_SYSTEM_EXT := system_ext
 TARGET_COPY_OUT_VENDOR := vendor
 TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
 
--include vendor/yaap/config/BoardConfigReservedSize.mk
-
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := taro
