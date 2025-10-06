36f5a3bc8c39f2a83b8ff8dad4ab92e1a2506b07
Fiqri Ardyansyah <fiqri191002@gmail.com>
2025-10-06 04:16:39 +0000

    marble: Rearrange overlays
    Change-Id: I6d83640a0f2bdc89a1bd66f5931b640cbf204b92
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- device.mk
+++ device.mk
@@ -287,18 +287,20 @@ PRODUCT_PACKAGES += \
     SettingsResMarble \
     SystemUIResMarble \
     WifiResMarble \
-    WifiResMarbleCN \
+    WifiResMarbleCN
 
 PRODUCT_PACKAGES += \
-    DialerResXiaomi \
     FrameworksResTarget \
-    FrameworksResXiaomi \
-    LineageResXiaomi \
-    SettingsProviderResXiaomi \
-    SettingsResXiaomi \
     WifiResTarget \
     WifiResTarget_cape \
     WifiResTarget_spf
+
+PRODUCT_PACKAGES += \
+    DialerResXiaomi \
+    FrameworksResXiaomi \
+    LineageResXiaomi \
+    SettingsProviderResXiaomi \
+    SettingsResXiaomi
 
 PRODUCT_PACKAGES += \
     NcmTetheringOverlay
