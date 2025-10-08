eeda884cf97dd083a9d00878127aa46b06d04c62
Fiqri Ardyansyah <fiqri191002@gmail.com>
2025-10-08 01:12:04 +0000

    marble: Standardize qca6490 WiFi config filename and symlink names
    Since we already dropped WiFi support for qca6750, there is no need
    to add `_qca6490` to name of config file and firmware symlinks.
    
    Change-Id: I0bd9ceef252134b0b4529307d16f798a4f96fc16
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- device.mk
+++ device.mk
@@ -497,7 +497,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wlan/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
-    $(LOCAL_PATH)/wlan/WCNSS_qcom_cfg_qca6490.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/qca6490/WCNSS_qcom_cfg.ini \
+    $(LOCAL_PATH)/wlan/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/qca6490/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wlan/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
 PRODUCT_COPY_FILES += \
@@ -509,5 +509,5 @@ PRODUCT_COPY_FILES += \
 
 # WiFi firmware symlinks
 PRODUCT_PACKAGES += \
-    firmware_qca6490_WCNSS_qcom_cfg.ini_symlink \
-    firmware_qca6490_wlan_mac.bin_symlink
+    firmware_WCNSS_qcom_cfg.ini_symlink \
+    firmware_wlan_mac.bin_symlink
